9f82616c04e0a8e57232a3354b9600fbafab9ba2
variousnabil <varionabilr@gmail.com>
2020-07-02 14:45:33 +0700

    first commit

---
+++ app/src/main/java/com/example/motionlayouttry/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.motionlayouttry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
